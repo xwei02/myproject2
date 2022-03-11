e6b913ab45c06d2aa9d04de0ac37123c1e66e906
User <sylvie.wu@hotmail.com>
2022-03-11 15:29:00 +0800

    initial commit

--- src/main/java/com/sylvie/Scoring.java
+++ src/main/java/com/sylvie/Scoring.java
@@ -2,9 +2,10 @@ package com.sylvie;
 
 public class Scoring {
     public static void main(String[] args) {
-        Student jack = new Student();
-        jack.name = "Jack";
-        jack.english = 80;
-        jack.maths = 64;
+        Student jack = new Student( "Jack", 40, 64);
+        Student sylvie =new Student("Sylvie", 80, 70);
+        Student s = new Student();
+        jack.print();
+        sylvie.print();
     }
 }
--- src/main/java/com/sylvie/Student.java
+++ src/main/java/com/sylvie/Student.java
@@ -4,4 +4,29 @@ public class Student {
     String name;
     int english;
     int maths;
+    public Student(String name){
+        this.name = name;
+    }
+    public Student(String name, int english, int maths){
+        this.name =name;
+        this.english = english;
+        this.maths = maths;
+    }
+    public Student(){
+        this("John Doe", -1, -1);
+        /*this.name = John Doe;
+        english = -1
+        maths = -1 */
+
+
+    }
+    public void print(){
+        int average = (english+maths)/2;
+        System.out.print(name + "\t" + english + "\t" +
+                maths + "\t" + average);
+        if (average<60){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 }
