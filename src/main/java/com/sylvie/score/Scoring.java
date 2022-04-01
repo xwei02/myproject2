97dfa6374fc9e11822936ab9da32bbe2d3f67e0c
user <sylvie.wu@hotmail.com>
2022-04-01 23:20:28 +0800

    commit

--- src/main/java/com/sylvie/score/Scoring.java
+++ src/main/java/com/sylvie/score/Scoring.java
@@ -7,6 +7,9 @@ public class Scoring {
     public static void main(String[] args) {
         List<Student> scores = new ArrayList<>();
         scores.add(new Student("Jack", 40, 64));
+        scores.add(new Student("Sylvie", 80, 70));
+        scores.add(new GraduateStudent("Jane", 64, 70, 85));
+        /*
         Student jack = new Student( "Jack", 40, 64);
         Student sylvie =new Student("Sylvie", 80, 70);
         GraduateStudent jane =
@@ -14,5 +17,12 @@ public class Scoring {
         jack.print();
         sylvie.print();
         jane.print();
+        */
+        for (int i = 0; i < 3; i++) {
+            scores.get(i).print();
+        }
+        for (Student s: scores){
+            s.print();
+        }
     }
 }
