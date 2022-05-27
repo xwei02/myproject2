e88517ae267c49b2178ec6805d9b5685d7020f1f
user <sylvie.wu@hotmail.com>
2022-05-27 16:39:17 +0800

    Homework1 gui game update

--- src/main/java/com/sylvie/ui/GuessFrame.java
+++ src/main/java/com/sylvie/ui/GuessFrame.java
@@ -5,14 +5,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame extends JFrame {
     //Fields
-    JButton button = new JButton("Hi");
+    JButton button = new JButton("Guess");
     JLabel label = new JLabel("Zzzz...");
+    JTextField number = new JTextField(8);
+    int secret = new Random().nextInt(10)+1;
     //Constructors*
     public GuessFrame() {
         super();
+        System.out.println("secret: "+secret);
         setSize(600, 400);
         setLocation(200, 300);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -20,11 +24,21 @@ public class GuessFrame extends JFrame {
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                int num = Integer.parseInt(number.getText());
+                System.out.println(num);
+                if (num>secret){
+                    label.setText("Smaller");
+                }else if (num<secret){
+                    label.setText("Bigger");
+                }else {
+                    label.setText("Bingo");
+                }
     //            System.out.println("Hello!");
-                label.setText("Hello!");
+    //            label.setText("Hello!");
             }
         });
         setLayout(new FlowLayout());
+        add(number);
         add(button);
         add(label);
         setVisible(true);
